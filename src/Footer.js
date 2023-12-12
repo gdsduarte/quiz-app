2c466144d5987b1970be4965ab8147916117e00f
gdsduarte <gds_duarte@hotmail.com>
2023-12-12 21:12:52 +0000

    First Commit

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+// Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Created by <b>Guilherme</b> (ID: <b>25662</b>)</p>
+    </div>
+  );
+};
+
+export default Footer;
